f0be9f14378bfdb436ebfb878e1fb336a442b0e4
John Mason <john@johnmason.io>
2017-04-04 16:54:40 -0400

    Complete fetch user from api

--- app/server.rb
+++ app/server.rb
@@ -1,6 +1,11 @@
 Bundler.require(:default)
 require_relative "interactions/record_github_event"
 
+if ENV["RACK_ENV"] != "production"
+  require "dotenv"
+  Dotenv.load
+end
+
 module GitHubWebhooks
   class Server < Sinatra::Base
     get "/events" do
